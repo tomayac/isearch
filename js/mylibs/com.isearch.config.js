66dd01585f234049d8b59516e902682ee131f4b4
Arnaud Brousseau <arnaudb@google.com>
2011-06-17 10:49:36 +0200

    Now querying YQL for real. Refactoring. Add a config object

---
+++ js/mylibs/com.isearch.config.js
@@ -0,0 +1,32 @@
+//Namespace
+var com;
+if (!com) {
+  com = {};
+} else if (typeof com != "object") {
+  throw new Error("com already exists and is not an object");
+}
+if (!com.isearch) {
+  com.isearch = {}
+} else if (typeof com.isearch != "object") {
+  throw new Error("com.isearch already exists and is not an object");
+}
+if (com.isearch.results) {
+  throw new Error("com.isearch.config already exists");
+}
+com.isearch.config = {
+  
+  //Query parameters
+  maxNumResults: 100, 
+  clusters0: 5,
+  clusters1: 3, 
+  trans: "rand", //Can be "lle" or "rand" 
+  
+  //Visualization parameters
+  visualizationMethod: "tmap", //tmap, htree or hpan
+  iconSize: 32, //16, 32, 48, 64
+
+  //Misc
+  flashLoadedTimeout: 2000 //wait until flash obj is loaded
+
+};
+
