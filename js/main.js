ed89527f42a49ee046aa156efb48dc8b9e641dc7
Arnaud Brousseau <arnaudb@google.com>
2011-06-23 13:07:04 +0200

    Centered the menu. Added Tokeniput files

--- js/main.js
+++ js/main.js
@@ -30,6 +30,23 @@ $(document).ready(function(){
     }
   });
 
+  //Behaviour of text field -- tokenizer
+  // $("#query-field").tokenInput([
+  //               {id: 7, name: "Ruby"},
+  //               {id: 11, name: "Python"},
+  //               {id: 13, name: "JavaScript"},
+  //               {id: 17, name: "ActionScript"},
+  //               {id: 19, name: "Scheme"},
+  //               {id: 23, name: "Lisp"},
+  //               {id: 29, name: "C#"},
+  //               {id: 31, name: "Fortran"},
+  //               {id: 37, name: "Visual Basic"},
+  //               {id: 41, name: "C"},
+  //               {id: 43, name: "C++"},
+  //               {id: 47, name: "Java"}
+  //           ], {theme: "facebook"});
+
+
   //Close button of the panel
   $('.panel footer a').click(function(){
     $('.panel').slideUp(200);
