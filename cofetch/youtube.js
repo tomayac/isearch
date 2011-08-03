e75f0dab067c11a965123da963a9e37bf3930019
Arnaud Brousseau <arnaudb@google.com>
2011-08-03 12:25:44 +0200

    Added youtube.js

---
+++ cofetch/youtube.js
@@ -0,0 +1,81 @@
+var nodeio = require('node.io');
+
+var methods = {
+    input: false,
+    run: function() {
+      
+      //Let's get the arguments passed to the script
+      if (!this.options.args[0]) {
+        this.exit('No arguments were given to the Youtube job');
+      }
+      var query = this.options.args[0];
+      
+      var maxResults = 10;
+      var youtubeURL = "https://gdata.youtube.com/feeds/api/videos?"
+          + 'q=' + query
+          + '&orderby=published'
+          + '&max-results=' + maxResults
+          + '&v=2'
+          + '&alt=json';
+        
+      //Let's go and request our content
+      this.get(youtubeURL, function(error, data, headers) {
+        
+        //Exit if there was a problem with the request
+        if (error) {
+           this.exit(err); 
+        }
+        
+        //Function to get tags
+        var getTags = function(videoEntry) {
+          var categories = videoEntry['category'];
+          var tags = [];
+          if (categories) {
+            var i
+            //Loop through the categories. NB: the first item is skipped because it's always a URL
+            for (i=1; i<categories.length; i++) {
+              tags.push(categories[i].term);
+            }
+          } else {
+            return [];
+          }
+        };
+
+
+        var youtubeResponse = JSON.parse(data);
+        var videos = youtubeResponse['feed']['entry'];
+        
+        //Declaration of the results array
+        var results = [];
+        
+        var i;
+        var result;
+        //let's loop through the array of videos
+        for (i=0;i<10;i++) {
+          result = {
+            "Type": "VideoType",
+            "Name": videos[i]['title']['$t'],
+            "Tags": getTags(videos[i]),
+            "Extension": "",
+            "Licence": "All right reserved", 
+            "LicenceURL": "http://www.youtube.com",
+            "Author": videos[i].author[0].name['$t'],
+            "Date": videos[i].published['$t'],
+            "Size": "",
+            "URL": "https://www.youtube.com/watch?v="+videos[i]['media$group']['yt$videoid']['$t'],
+            "Preview": videos[i]['media$group']['media$thumbnail'][0].url,
+            "Dimensions": [],
+            "Length": videos[i]['media$group']['yt$duration'].seconds,
+            "Emotions": [],
+            "Location": [],
+            "Weather": {}
+          };
+          results.push(result);
+        }
+        
+        this.emit(results);
+      });
+    }
+}
+
+exports.job = new nodeio.Job({timeout:10}, methods);
