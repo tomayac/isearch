628834730963e7292bd16279bfe65e43c7b1e370
Jonas Etzold <jonas.etzold@fh-erfurt.de>
2011-08-04 16:32:54 +0200

    Final version of flickr.js (finally found a way to get it work)

--- cofetch/flickr.js
+++ cofetch/flickr.js
@@ -18,6 +18,18 @@ var methods = {
 		var apiKey = '5226908fe75b3dae6290f60162a501d5';
 		var maxResults = 30;
 		
+		var licenses = [
+	                   { "name": "All Rights Reserved", "url": "" },
+	                   { "name": "Attribution-NonCommercial-ShareAlike License", "url": "http://creativecommons.org/licenses/by-nc-sa/2.0/"},
+	                   { "name": "Attribution-NonCommercial License", "url": "http://creativecommons.org/licenses/by-nc/2.0/"},
+	                   { "name": "Attribution-NonCommercial-NoDerivs License", "url": "http://creativecommons.org/licenses/by-nc-nd/2.0/" },
+	                   { "name": "Attribution License", "url": "http://creativecommons.org/licenses/by/2.0/" },
+	                   { "name": "Attribution-ShareAlike License", "url": "http://creativecommons.org/licenses/by-sa/2.0/" },
+	                   { "name": "Attribution-NoDerivs License", "url": "http://creativecommons.org/licenses/by-nd/2.0/" },
+	                   { "name": "No known copyright restrictions", "url": "http://www.flickr.com/commons/usage/" },
+	                   { "name": "United States Government Work", "url": "http://www.usa.gov/copyright.shtml" }
+	               ];
+
 		var searchURL = "http://api.flickr.com/services/rest/?"
 			+ 'method=flickr.photos.search'
 			+ '&api_key=' + apiKey
@@ -50,7 +62,7 @@ var methods = {
 				//Get the image info for the current image
 				var infoURL = "http://api.flickr.com/services/rest/?"
 					+ 'method=flickr.photos.getInfo'
-					+ '&api_key=' + context.apiKey
+					+ '&api_key=' + apiKey
 					+ '&photo_id=' + photoId
 					+ '&format=json'
 					+ '&nojsoncallback=1';
@@ -65,13 +77,14 @@ var methods = {
 					var infoData = JSON.parse(data).photo;
 					
 					var tags = new Array;
+					
 					for(var t=0; t < infoData.tags.tag.length; t++) {
 						tags.push(infoData.tags.tag[t]._content);
 					}
 					
 					var result = {
 						"Type": "ImageType",
-						"Name": infoData.title,
+						"Name": infoData.title._content,
 						"Tags": tags,
 						"Extension": infoData.originalformat || 'jpg',
 						"Licence": licenses[infoData.license].name, 
@@ -89,8 +102,8 @@ var methods = {
 					//Get the image sizes for the current image
 					var sizesURL = "http://api.flickr.com/services/rest/?"
 						+ 'method=flickr.photos.getSizes'
-						+ '&api_key=' + context2.apiKey
-						+ '&photo_id=' + photoId
+						+ '&api_key=' + apiKey
+						+ '&photo_id=' + infoData.id
 						+ '&format=json'
 						+ '&nojsoncallback=1';
 					
@@ -107,15 +120,20 @@ var methods = {
 						var sizeData = JSON.parse(data).sizes.size;
 						var sizecount = sizeData.length;
 						
+						//Get the biggest available image index
+						var sizeindex = sizecount-1;
+						if(sizeData[sizeindex].label == 'Original') {
+							sizeindex -= 1;
+						}
+						
 						result.Preview = sizeData[0].source;
-						result.URL     = sizeData[sizecount-1].source;
-						result.Size    = {'width' :sizeData[sizecount-1].width,
-					                      'height':sizeData[sizecount-1].height};
+						result.URL     = sizeData[sizeindex ].source;
+						result.Size    = {'width' :sizeData[sizeindex ].width,
+					                      'height':sizeData[sizeindex].height};
 						
-						console.log(result);
-						context.results.push(result);
+						results.push(result);
 						
-						if (context.results.length===context.maxResults) {
+						if (results.length===maxResults) {
 							//Exit the job if we're done, i.e Array full
 							context.emit();
 			            }
@@ -133,99 +151,3 @@ var job = new nodeio.Job({timeout:10}, methods);
 exports.fetch = function(query, results, callback) {
 	nodeio.start(job, {args: [query, results]}, callback);
 };
-/*
-var FlickrAPI= require('flickrnode').FlickrAPI,
-    sys= require('sys'),
-    flickr = new FlickrAPI('5226908fe75b3dae6290f60162a501d5', 'cc06237000c66b6c');
-
-this.fetch = function(query) {
-
-	var q = query.replace(' ','+');
-	var photoInfo = new Array;
-	var licenses = [
-	                   { "name": "All Rights Reserved", "url": "" },
-	                   { "name": "Attribution-NonCommercial-ShareAlike License", "url": "http://creativecommons.org/licenses/by-nc-sa/2.0/"},
-	                   { "name": "Attribution-NonCommercial License", "url": "http://creativecommons.org/licenses/by-nc/2.0/"},
-	                   { "name": "Attribution-NonCommercial-NoDerivs License", "url": "http://creativecommons.org/licenses/by-nc-nd/2.0/" },
-	                   { "name": "Attribution License", "url": "http://creativecommons.org/licenses/by/2.0/" },
-	                   { "name": "Attribution-ShareAlike License", "url": "http://creativecommons.org/licenses/by-sa/2.0/" },
-	                   { "name": "Attribution-NoDerivs License", "url": "http://creativecommons.org/licenses/by-nd/2.0/" },
-	                   { "name": "No known copyright restrictions", "url": "http://www.flickr.com/commons/usage/" },
-	                   { "name": "United States Government Work", "url": "http://www.usa.gov/copyright.shtml" }
-	               ];
-
-	flickr.photos.search({'text':q,'per_page':30},  function(error, results) {
-		if(error){
-	      return {'code':100,'body':'Whoops, something went wrong. Try that again.'};
-	    }else{
-
-	      var end = false;	
-	      // Store photoIDs. Note: These come back in reverse chronological order.
-	      for (var x=0; x < results.photo.length; x++) {
-	        photoInfo.push({
-	          'Type':'ImageType',	
-	          'Name':'',
-	          'Tags':'',
-	          'Extension':'',
-	          'License':'',
-	          'LicenseURL' :'',
-	          'Author' :'',
-	          'Date' :'',
-	          'Size':'',
-	          'URL':'',
-	          'Preview':'',
-	          'Emotions':'',
-	          'Location':'',
-	          'Weather':''
-	        });
-	        };
-
-	      // Get photo information
-
-	      // Loop so that we keep track of the photo order
-	      for (var x=0; x < photoInfo.length; x++) {
-
-	        flickr.photos.getSizes(results.photo[x].id, x, function(error2, sizes, idx){
-				// Populate array at a specific location, because results come back as fast as possible (not in order)
-				if(!error2){
-					var sizecount = sizes.size.length;
-					photoInfo[x].Preview = sizes.size[0].source;
-					photoInfo[x].URL = sizes.size[sizecount-1].source;
-					photoInfo[x].Size = {'width' :sizes.size[sizecount-1].width,
-							             'height':sizes.size[sizecount-1].height};
-				}
-	        });
-
-	        //Get taken date, tags etc.
-	        flickr.photos.getInfo(results.photo[x].id, '', function(error3, info, idx) {
-	        	if(!error3){
-		            photoInfo[x].Name = info.title;
-		            photoInfo[x].Author = info.owner.realname || info.owner.username;
-		            photoInfo[x].Date = info.dates.taken;
-		            photoInfo[x].Extension = info.originalformat || 'jpg';
-		            photoInfo[x].License = licenses[info.license].name;
-		            photoInfo[x].LicenseURL = licenses[info.license].url;
-		            photoInfo[x].Location = [info.location.latitude || 0 ,info.location.longitude || 0,0,0];
-
-		            var tags = new Array;
-		            for(var t=0; t < info.tags.tag.length; t++) {
-		            	tags.push(info.tags.tag[t]._content);
-		            }
-		            photoInfo[x].Tags = tags;
-		            console.log(photoInfo);
-		            if(x == (photoInfo.length-1)) {
-		            	end = true;
-		            }
-		        }
-	        });
-
-	      }; // for
-
-	      if(end) {
-	    	  console.log(photoInfo);
-	    	  return {'code':200, 'body':photoInfo};
-	      }
-	    } // else
-	});
-
-};*/
