e1fe292a11c3d2f55633d34b2458b3ecfa9782b2
Jonas Etzold <jonas.etzold@fh-erfurt.de>
2011-09-13 19:22:02 +0200

    Fixed a problem with CO JSON files without video data.

--- cofetch/store.js
+++ cofetch/store.js
@@ -136,12 +136,17 @@ var publishRUCoD = function(data, outputPath, callback) {
 	
 	var rucodBody = '<Tags>';
 	
+	var hasVideo = false;
 	var tagArray = new Array();
 	//Grabbing the tags for the RUCoD header
 	for(var f=0; f < data.Files.length; f++) {
 		//Text has no tags ;-)
 		if(data.Files[f].Type == 'Text') {
 			continue;
+		};
+		//We need to know if there is video since the way to get the video source url is quite complicated
+		if(data.Files[f].Type == 'Video') {
+			hasVideo = true;
 		};
 		for(var t=0; t < data.Files[f].Tags.length; t++) {
 			tagArray.push(data.Files[f].Tags[t]);
@@ -154,154 +159,166 @@ var publishRUCoD = function(data, outputPath, callback) {
 		rucodBody += '<MetaTag name="UserTag" xsi:type="xsd:string">' + uniqueTags[t] + '</MetaTag>';
 	}
 	
-	rucodBody += '</Tags>' +
-                 '<ContentObjectTypes>';
-
-	//First run, get the video data url for youtube videos 
-	for(var f=0; f < data.Files.length; f++) {
-		if (data.Files[f].Type == 'VideoType') {
-			getVideoSourceUrl(data.Files[f].URL, f, function(error, id, url) {
+	rucodBody += '</Tags>';
+	
+	//------------------------------------------------
+	var saveRucodMedia = function(rucodBody, data, outputPath, callback) {
+		
+		rucodBody += '<ContentObjectTypes>';
+		
+		//Fitting the media files into RUCoD
+		for(var f=0; f < data.Files.length; f++) {
+			rucodBody += '<MultimediaContent xsi:type="' + data.Files[f].Type + '">';
 			
-				data.Files[id].URL = url;
+			if(data.Files[f].Type == 'Text') {
+				rucodBody += '<FreeText>' + data.Files[f].FreeText + '</FreeText>';
+			} else  {	
 				
-				//Fitting the media files into RUCoD
-				for(var f=0; f < data.Files.length; f++) {
-					rucodBody += '<MultimediaContent xsi:type="' + data.Files[f].Type + '">';
-					
-					if(data.Files[f].Type == 'Text') {
-						rucodBody += '<FreeText>' + data.Files[f].FreeText + '</FreeText>';
-					} else  {	
-						
-						if(mime[data.Files[f].Extension]) {
-							rucodBody += '<FileFormat>' + mime[data.Files[f].Extension] + '</FileFormat>';
-						}
-						
-						if(data.Files[f].Description) {
-							rucodBody += '<FreeText>' + data.Files[f].Description + '</FreeText>';
-						}
-						
-						rucodBody += '<MediaCreationInformation>';
-						rucodBody += '<Author>';
-						rucodBody += '<Name>' + data.Files[f].Author + '</Name>';
-						rucodBody += '</Author>';
-						rucodBody += '<Licensing>' + data.Files[f].License + '</Licensing>';
-						rucodBody += '</MediaCreationInformation>';
-						
-						rucodBody += '<MediaLocator>';
-						rucodBody += '<MediaUri>' + data.Files[f].URL + '</MediaUri>';
-						rucodBody += '<MediaPreview>' + data.Files[f].Preview + '</MediaPreview>';
-						rucodBody += '</MediaLocator>';
-						
-						rucodBody += '<MediaName>' + data.Files[f].Name + '</MediaName>';
-						
-						if(data.Files[f].Length) {
-							rucodBody += '<MediaTime>' + data.Files[f].Length + '</MediaTime>';
-						}
-						
-						for(var t=0; t < data.Files[f].Tags.length; t++) {
-							rucodBody += '<MetaTag name="UserTag" xsi:type="xsd:string">' + data.Files[f].Tags[t] + '</MetaTag>';
-						}
-							
-					} 
-					
-					rucodBody += '</MultimediaContent>';
-					
-				} // End for loop
-				
-				rucodBody += '</ContentObjectTypes>';
-				
-				//Generate RWML data for each media item
-				var rwml = '<RWML>';
-				
-				for(var f=0; f < data.Files.length; f++) {
-					//Text does not has any real world information
-					if(data.Files[f].Type == 'Text') {
-						continue;
-					}
-					//Prepare the creation date of the media item for use in RWML 
-					var rawDateParts = data.Files[f].Date.split(" ");
-					var dateParts = (rawDateParts[0] || "2011-08-01").split("-");
-					var timeParts = (rawDateParts[1] || "12:01:01").split(":");
-					var rwmlDate = new Date(parseInt(dateParts[0]),parseInt(dateParts[1].replace(/0(\d\/)/g,'')),parseInt(dateParts[2].replace(/0(\d\/)/g,'')),parseInt(timeParts[0].replace(/0(\d\/)/g,'')),parseInt(timeParts[1].replace(/0(\d\/)/g,'')),parseInt(timeParts[2].replace(/0(\d\/)/g,'')));
-					
-					//Create the RWML file
-					   rwml += '<ContextSlice>' +
-					           '<DateTime>' +
-					           '<Date>' + getISODateString(rwmlDate) + '</Date>' +	
-					           '</DateTime>';
-					//Do we have GPS
-					if(data.Files[f].Location.length > 1) {
-					   var location = data.Files[f].Location;	
-					   rwml += '<Location type="gml">' +
-					           '<gml:CircleByCenterPoint numArc="1">' +
-					           '<gml:pos>' + location[0] + ' ' + location[1] + '</gml:pos>' +
-					           '<gml:radius uom="M">10</gml:radius>' +
-					           '</gml:CircleByCenterPoint>' +
-					           '</Location>' +
-					           '<Direction>' +
-					           '<Heading>' + location[3] + '</Heading>' +
-					           '<Tilt>0</Tilt>' +
-					           '<Roll>0</Roll>' +
-					           '</Direction>';
-					}
-					//Do we have weather
-					if(data.Files[f].Weather.temperature.length > 1) {
-					   rwml += '<Weather>' +
-	                           '<Condition>' + data.Files[f].Weather.condition + '</Condition>' +
-	                           '<Temperature>' + data.Files[f].Weather.temperature + '</Temperature>' +
-	                           '<WindSpeed>' + data.Files[f].Weather.wind + '</WindSpeed>' +
-	                           '<Humidity>' + data.Files[f].Weather.humidity + '</Humidity>' +
-	                           '</Weather>';	
-					}
-					
-					   rwml += '</ContextSlice>';
-				} // End for loop for RWML
-				
-				rwml += '</RWML>';
-				
-				
-				//Add the RWML to the RUCoD
-				rucodBody += '<RealWorldInfo>' +
-							 '<MetadataUri filetype="rwml">' + publicOutputUrl + '/' + baseName + '.rwml</MetadataUri> ' +
-							 '</RealWorldInfo>';   
-				
-				//Find and add emotion if there are some
-				var emoIndex = 0;
-				
-				for(var f=0; f < data.Files.length; f++) {
-					if(data.Files[f].Type == 'Text') {
-						continue;
-					}
-					if(data.Files[f].Emotions.length > 0 && data.Files[f].Emotions[0] != "Not set") {
-						emoIndex = f;
-						break;
-					}
+				if(mime[data.Files[f].Extension]) {
+					rucodBody += '<FileFormat>' + mime[data.Files[f].Extension] + '</FileFormat>';
 				}
-				rucodBody += '<UserInfo>' +
-							 '<UserInfoName>Emotion</UserInfoName>';
-				for(var e=0; e < data.Files[emoIndex].Emotions.length; e++) {
-					rucodBody += '<emotion><category set="everydayEmotions" name="' + data.Files[emoIndex].Emotions[e] + '"/></emotion>';
-				}
-			    rucodBody += '</UserInfo>';	
-			    
-			    console.log("Temporary RUCoD data collected. Writing files...");
-				//Write RWML file
-				fs.writeFile(outputPath+ baseName + '.rwml', rwml, function (error) {
-					if (error) throw error;
-					console.log('RWML file created or overwritten under ' + outputPath + baseName + '.rwml');
-					
-					//Write RUCoD file
-					fs.writeFile(outputPath + baseName + '_rucod.xml', (rucodHeadS + rucodBody + rucodHeadE), function (error) {
-						if (error) throw error;
-						console.log('RUCoD file created or overwritten under ' + outputPath + baseName + '_rucod.xml');
-						
-						callback('JSON and RUCoD files successfully saved.');
-					});
-				});
 				
-			}); //End asynchronous call
-		}	
-	} //End first run loop
+				if(data.Files[f].Description) {
+					rucodBody += '<FreeText>' + data.Files[f].Description + '</FreeText>';
+				}
+				
+				rucodBody += '<MediaCreationInformation>';
+				rucodBody += '<Author>';
+				rucodBody += '<Name>' + data.Files[f].Author + '</Name>';
+				rucodBody += '</Author>';
+				rucodBody += '<Licensing>' + data.Files[f].License + '</Licensing>';
+				rucodBody += '</MediaCreationInformation>';
+				
+				rucodBody += '<MediaLocator>';
+				rucodBody += '<MediaUri>' + data.Files[f].URL + '</MediaUri>';
+				rucodBody += '<MediaPreview>' + data.Files[f].Preview + '</MediaPreview>';
+				rucodBody += '</MediaLocator>';
+				
+				rucodBody += '<MediaName>' + data.Files[f].Name + '</MediaName>';
+				
+				if(data.Files[f].Length) {
+					rucodBody += '<MediaTime>' + data.Files[f].Length + '</MediaTime>';
+				}
+				
+				for(var t=0; t < data.Files[f].Tags.length; t++) {
+					rucodBody += '<MetaTag name="UserTag" xsi:type="xsd:string">' + data.Files[f].Tags[t] + '</MetaTag>';
+				}
+					
+			} 
+			
+			rucodBody += '</MultimediaContent>';
+			
+		} // End for loop
+		
+		rucodBody += '</ContentObjectTypes>';
+		
+		//Generate RWML data for each media item
+		var rwml = '<RWML>';
+		
+		for(var f=0; f < data.Files.length; f++) {
+			//Text does not has any real world information
+			if(data.Files[f].Type == 'Text') {
+				continue;
+			}
+			//Prepare the creation date of the media item for use in RWML 
+			var rawDateParts = data.Files[f].Date.split(" ");
+			var dateParts = (rawDateParts[0] || "2011-08-01").split("-");
+			var timeParts = (rawDateParts[1] || "12:01:01").split(":");
+			var rwmlDate = new Date(parseInt(dateParts[0]),parseInt(dateParts[1].replace(/0(\d\/)/g,'')),parseInt(dateParts[2].replace(/0(\d\/)/g,'')),parseInt(timeParts[0].replace(/0(\d\/)/g,'')),parseInt(timeParts[1].replace(/0(\d\/)/g,'')),parseInt(timeParts[2].replace(/0(\d\/)/g,'')));
+			
+			//Create the RWML file
+			   rwml += '<ContextSlice>' +
+			           '<DateTime>' +
+			           '<Date>' + getISODateString(rwmlDate) + '</Date>' +	
+			           '</DateTime>';
+			//Do we have GPS
+			if(data.Files[f].Location.length > 1) {
+			   var location = data.Files[f].Location;	
+			   rwml += '<Location type="gml">' +
+			           '<gml:CircleByCenterPoint numArc="1">' +
+			           '<gml:pos>' + location[0] + ' ' + location[1] + '</gml:pos>' +
+			           '<gml:radius uom="M">10</gml:radius>' +
+			           '</gml:CircleByCenterPoint>' +
+			           '</Location>' +
+			           '<Direction>' +
+			           '<Heading>' + location[3] + '</Heading>' +
+			           '<Tilt>0</Tilt>' +
+			           '<Roll>0</Roll>' +
+			           '</Direction>';
+			}
+			//Do we have weather
+			if(data.Files[f].Weather.temperature.length > 1) {
+			   rwml += '<Weather>' +
+                       '<Condition>' + data.Files[f].Weather.condition + '</Condition>' +
+                       '<Temperature>' + data.Files[f].Weather.temperature + '</Temperature>' +
+                       '<WindSpeed>' + data.Files[f].Weather.wind + '</WindSpeed>' +
+                       '<Humidity>' + data.Files[f].Weather.humidity + '</Humidity>' +
+                       '</Weather>';	
+			}
+			
+			   rwml += '</ContextSlice>';
+		} // End for loop for RWML
+		
+		rwml += '</RWML>';
+		
+		
+		//Add the RWML to the RUCoD
+		rucodBody += '<RealWorldInfo>' +
+					 '<MetadataUri filetype="rwml">' + publicOutputUrl + '/' + baseName + '.rwml</MetadataUri> ' +
+					 '</RealWorldInfo>';   
+		
+		//Find and add emotion if there are some
+		var emoIndex = 0;
+		
+		for(var f=0; f < data.Files.length; f++) {
+			if(data.Files[f].Type == 'Text') {
+				continue;
+			}
+			if(data.Files[f].Emotions.length > 0 && data.Files[f].Emotions[0] != "Not set") {
+				emoIndex = f;
+				break;
+			}
+		}
+		rucodBody += '<UserInfo>' +
+					 '<UserInfoName>Emotion</UserInfoName>';
+		for(var e=0; e < data.Files[emoIndex].Emotions.length; e++) {
+			rucodBody += '<emotion><category set="everydayEmotions" name="' + data.Files[emoIndex].Emotions[e] + '"/></emotion>';
+		}
+	    rucodBody += '</UserInfo>';	
+	    
+	    console.log("Temporary RUCoD data collected. Writing files...");
+		//Write RWML file
+		fs.writeFile(outputPath+ baseName + '.rwml', rwml, function (error) {
+			if (error) throw error;
+			console.log('RWML file created or overwritten under ' + outputPath + baseName + '.rwml');
+			
+			//Write RUCoD file
+			fs.writeFile(outputPath + baseName + '_rucod.xml', (rucodHeadS + rucodBody + rucodHeadE), function (error) {
+				if (error) throw error;
+				console.log('RUCoD file created or overwritten under ' + outputPath + baseName + '_rucod.xml');
+				
+				callback('JSON and RUCoD files successfully saved.');
+			});
+		});
+	}; //Function saveRucodMedia end
+	
+	if(hasVideo) {
+		//First run, get the video data url for youtube videos 
+		for(var f=0; f < data.Files.length; f++) {
+			if (data.Files[f].Type == 'VideoType') {
+				getVideoSourceUrl(data.Files[f].URL, f, function(error, id, url) {
+				
+					data.Files[id].URL = url;
+					saveRucodMedia(rucodBody, data, outputPath, callback);
+					
+				}); //End asynchronous call
+			} 
+		} //End first run loop
+		
+	} else {
+		//No video in JSON so don't go through the video source url retrieval path
+		saveRucodMedia(rucodBody, data, outputPath, callback);
+	}
 };
 
 
