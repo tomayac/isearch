48014bcf6ac300a13470b525638fa6fb7b81ee22
Jonas Etzold <jonas.etzold@fh-erfurt.de>
2011-09-20 16:14:13 +0200

    pTag - Added first functions and descriptions

--- server/ptag/ptag.js
+++ server/ptag/ptag.js
@@ -12,22 +12,37 @@ this.name = "pTag";
 this.version = "0.1.0";
 this.endpoint = "http://isearch.ai.fh-erfurt.de/ptag";
 
-exports.echo = function(options, callback){
-  callback(null, options.msg);
+exports.tagRecommendations = function(options, callback){
+	var tags = ['tag1','tag2','tag3','tag4','tag5','tag6','tag7','tag8','tag9','tag10'];
+	callback(null, tags);
 };
-//Documentation for echo function
-exports.echo.description = "this is the echo method, it echos back your msg";
-exports.echo.schema = {
-  msg: { 
-    type: 'string',
+//Documentation for tagRecommendations function
+exports.tagRecommendations.description = "This method generates tag recommendations for search queries based on a user profile. The user profile is gathered through the provided user ID.";
+exports.tagRecommendations.schema = {
+  userID: { 
+    type: 'int',
     optional: false 
   }
 };
 
-exports.ping = function(options, callback){
+exports.filterTags = function(options, callback){
   setTimeout(function(){
     callback(null, 'pong');
   }, 2000);
 };
 //Documentation for ping function
-exports.ping.description = "this is the ping method, it pongs back after a 2 second delay";
+exports.filterTags.description = "This method generates tag recommendations as filter tags for a given result set based on a user profile. A user ID as well as the textual parts of the user query needs to be provided.";
+exports.filterTags.schema = {
+  userID: { 
+    type: 'int',
+    optional: false 
+  },
+  query: { 
+    type: 'string',
+    optional: false 
+  },
+  resultSet: { 
+    type: 'object',
+    optional: false 
+  }
+};
