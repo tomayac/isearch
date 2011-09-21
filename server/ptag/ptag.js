d57868558f4358bddfb374d4a3c525a2768c7a94
Jonas Etzold <jonas.etzold@fh-erfurt.de>
2011-09-21 11:28:59 +0200

    added all external reachable service functions to pTag

--- server/ptag/ptag.js
+++ server/ptag/ptag.js
@@ -12,6 +12,9 @@ this.name = "pTag";
 this.version = "0.1.0";
 this.endpoint = "http://isearch.ai.fh-erfurt.de/ptag";
 
+/**
+ * tagRecommendations function
+ */
 exports.tagRecommendations = function(options, callback){
 	var tags = ['tag1','tag2','tag3','tag4','tag5','tag6','tag7','tag8','tag9','tag10'];
 	callback(null, tags);
@@ -25,12 +28,14 @@ exports.tagRecommendations.schema = {
   }
 };
 
+/**
+ * filterTags function
+ */
 exports.filterTags = function(options, callback){
-  setTimeout(function(){
-    callback(null, 'pong');
-  }, 2000);
+	var tags = ['tag1','tag2','tag3','tag4','tag5','tag6','tag7','tag8','tag9','tag10'];
+	callback(null, tags);
 };
-//Documentation for ping function
+//Documentation for filterTags function
 exports.filterTags.description = "This method generates tag recommendations as filter tags for a given result set based on a user profile. A user ID as well as the textual parts of the user query needs to be provided.";
 exports.filterTags.schema = {
   userID: { 
@@ -41,8 +46,101 @@ exports.filterTags.schema = {
     type: 'string',
     optional: false 
   },
-  resultSet: { 
-    type: 'object',
+  resultSetTags: { 
+    type: 'array',
+    optional: false 
+  }
+};
+
+/**
+ * resultTagRecommendations function
+ */
+exports.resultTagRecommendations = function(options, callback){
+	var tags = ['tag1','tag2','tag3','tag4','tag5','tag6','tag7','tag8','tag9','tag10'];
+	callback(null, tags);
+};
+//Documentation for resultTagRecommendations function
+exports.resultTagRecommendations.description = "Serves a personalised tag list, specific for the requested result item. To be used before the “Download” event of a result item.";
+exports.resultTagRecommendations.schema = {
+  userID: { 
+    type: 'int',
+    optional: false 
+  },
+  query: { 
+    type: 'string',
+    optional: false 
+  },
+  resultItemTags: { 
+    type: 'array',
+    optional: false 
+  }
+};
+
+/**
+ * tag function
+ */
+exports.tag = function(options, callback){
+	var stored = false;
+	//storeUserTags(RUCoDID,Tags) options.tags
+	var rucodMangerURL = "http://www.isearch-project.eu/rmn/storeUserTags?" 
+		               + "rucodid=" + options.resultItemID + "&" 
+		               + "tags=" + options.tags;
+	
+	//Use node.io to post the data to external RUCoD manager
+	
+	//Return if the request succeeded or not
+	callback(null, stored);
+};
+//Documentation for tag function
+exports.tag.description = "Stores the provided tag in the RUCoD header for the provided Content Object provided via the resultItemID. To be used if a user supplies tags.";
+exports.tag.schema = {
+  userID: { 
+    type: 'int',
+    optional: false 
+  },
+  tags: { 
+    type: 'string',
+    optional: false 
+  },
+  resultItemID: { 
+    type: 'string',
+    optional: false 
+  }
+};
+
+/**
+ * implicitTags function
+ */
+exports.implicitTags = function(options, callback){
+	var stored = false;
+	//Extract the tags
+	//var queryTags = options.query.split(',');
+	//Filter user relevant tags
+	//...
+	
+	//Store tags with RUCoD Manager 
+	var rucodMangerURL = "http://www.isearch-project.eu/rmn/storeUserTags?" 
+		               + "rucodid=" + options.resultItemID + "&" 
+		               + "tags=" + options.tags;
+	
+	//Use node.io to post the data to external RUCoD manager
+	
+	//Return if the request succeeded or not
+	callback(null, stored);
+};
+//Documentation for implicitTags function
+exports.implicitTags.description = "Derives a tag for the resultItem based on the query and user profile and stores it in the referring RUCoD. To be used if a “Download” event occurs without a user specified tag.";
+exports.implicitTags.schema = {
+  userID: { 
+    type: 'int',
+    optional: false 
+  },
+  query: { 
+    type: 'string',
+    optional: false 
+  },
+  resultItemID: { 
+    type: 'string',
     optional: false 
   }
 };
