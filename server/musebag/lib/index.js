693613c3974d939bd05320e9e314ede3a30655b6
Jonas Etzold <jonas.etzold@fh-erfurt.de>
2011-09-27 17:45:07 +0200

    Quick fix

--- server/musebag/lib/index.js
+++ server/musebag/lib/index.js
@@ -58,6 +58,9 @@ app.get('/profile/:attrib', function(req, res) {
 	}
 	//Does the requested profile attribute is available
 	if(req.session.user[attrib]) {
+		var data = {};
+		data[attrib] = req.session.user[attrib];
+		console.log(data);
 		res.send('{"' + attrib + '": "' + req.session.user[attrib] + '"}');
 	} else {
 		res.send(JSON.stringify({error : 'The requested user profile attribute is not available!'}));
