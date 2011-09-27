3e5679ac452583d7e7f938c633f64440dea75390
Jonas Etzold <jonas.etzold@fh-erfurt.de>
2011-09-27 13:19:48 +0200

    replaced nodeio with restler for querying external webservices

--- server/musebag/lib/musebag.js
+++ server/musebag/lib/musebag.js
@@ -11,53 +11,35 @@
 /**
  * Required node modules
  */
-var nodeio = require('node.io');
+var restler = require('restler');
 
 var verifyUser = function(email,pw,callback) {
 	
-	var jobMethods = {
-			input: false,
-		    run: function() {
-		    	
-		    	//Let's get the arguments passed to the script
-		        if (!this.options.args[0]) {
-		          this.exit('No arguments were given to the dbpedia job');
-		        }
-		        
-		        var email = this.options.args[0];
-		        var pw    = this.options.args[1];
-		    	
-		    	var verifyURL = "http://gdv.fh-erfurt.de/i-search/apc-dummy/index.php?"
-		            + 'f=validateUser'
-		            + '&email=' + email
-		            + '&pw=' + pw;
-		    	
-		    	this.get(verifyURL, function(error, data, headers) {
-		            
-		            //Exit if there was a problem with the request
-		            if (error) {
-		               this.exit(error); 
-		            }
-		            
-		            var verifyResponse = JSON.parse(data);
-		            
-		            //Check if return data is ok
-		            if(verifyResponse.error) {
-		            	this.exit(verifyResponse.error);
-		            }
-		            if(!verifyResponse.user) {
-		            	this.exit('The user data was delivered in an invalid format.');
-		            }
-		            
-		            //Exit the Job and return the user data
-		            this.emit(verifyResponse.user);
-		    	}); //end of get
-		    }
-	}; // end of jobMethods
+	var verifyURL = "http://gdv.fh-erfurt.de/i-search/apc-dummy/index.php?"
+        + 'f=validateUser'
+        + '&email=' + email
+        + '&pw=' + pw;
 	
-	//Creates the job
-	var verifyJob = new nodeio.Job({timeout:10}, jobMethods);
-	nodeio.start(verifyJob, {args: [email,pw]}, callback, true);
+	rest.get(verifyURL)
+	.on('complete', function(data) {
+		console.log(data);
+		
+		//Check if return data is ok
+        if(data.error) {
+        	console.log(data.error);
+        	callback(data.error, null);
+        }
+        if(!data.user) {
+        	console.log('The user data was delivered in an invalid format.');
+        	callback('The user data was delivered in an invalid format.', null);
+        }
+        
+        callback(null, data.user);
+	})
+	.on('error', function(error) {
+		console.log(error);
+		callback(error, null);
+	});
 };
 
 /**
