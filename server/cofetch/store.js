8089660b1c4e3934cc088b8e00736f8b085f1f8e
Jonas Etzold <jonas.etzold@fh-erfurt.de>
2011-10-21 13:16:26 +0200

    investigating store error

--- server/cofetch/store.js
+++ server/cofetch/store.js
@@ -149,6 +149,7 @@ var publishRUCoD = function(data, outputPath, callback) {
 			hasVideo = true;
 		};
 	    
+		console.log(data.Files[f]);
 		for(var t=0; t < data.Files[f].Tags.length; t++) {
 				tagArray.push(data.Files[f].Tags[t]);
 		}
