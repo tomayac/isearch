7a2fd290a6c5852021141754ba5e80247c3a2f14
Jonas Etzold <jonas.etzold@fh-erfurt.de>
2011-11-30 11:02:14 +0100

    cofetch RUCoD adjustments

---
+++ server/cofetch/recreate.js
@@ -0,0 +1,34 @@
+var fs = require('fs'), nodeio = require('node.io'), rucod = require('./store');
+
+var basepath = '/var/www/isearch/client/cofetch/output';
+
+exports.job = new nodeio.Job({
+  recurse : true
+}, {
+  input : basepath,
+  run : function(full_path) {
+
+    console.log('Recreate file path       : ' + full_path);
+    var ext = (/[.]/.exec(filename)) ? /[^.]+$/.exec(filename) : undefined;
+    console.log('Recreate file extension  : ' + ext);
+
+    if (ext === 'json') {
+      fs.readFile(full_path, function(error, data) {
+        if (error) {
+          console.log('Recreate file read error : ' + error);
+        } else {
+          console.log(data);/*
+          rucod.store(data, true, true, false, function(error, data) {
+            if (error) {
+              console.log('Recreate store error     : ' + error);
+            } else {
+              console.log('Recreate store success   : ' + message);
+            }
+          });*/
+        }
+      });
+    }
+
+    this.emit();
+  }
+});
