e1762e2b3085a73d8e5878de6e37ae902223d25c
Jonas Etzold <jonas.etzold@fh-erfurt.de>
2012-02-03 09:49:47 +0100

    removed local.js from tracking to allow individual settings

--- client/musebag/js/mylibs/local.js
+++ client/musebag/js/mylibs/local.js
@@ -1,9 +1,9 @@
 function localConfig(data)
 {
 	data.visOptions.method = 'classic' ;
-	data.fileUploadServer = "http://vision.iti.gr/sotiris/isearch/upload.php" ;
-	data.queryFormulatorUrl = "http://vision.iti.gr/sotiris/isearch/client/musebag/mqf.php?index=iaprtc12&cls=3" ;
-	data.userProfileServerUrl = "http://vision.iti.gr/sotiris/isearch/client/musebag/user.php?mode=Profile&key=" ;
-	data.userLoginServerUrl = "http://vision.iti.gr/sotiris/isearch/client/musebag/user.php?mode=login" ;
+	data.fileUploadServer = "http://vision.iti.gr/sotiris/isearch/upload.php" ; //"query/item"; //
+	data.queryFormulatorUrl = "http://vision.iti.gr/sotiris/isearch/mqf.php?index=uc6"; //"query"; //"http://vision.iti.gr/sotiris/isearch/client/musebag/mqf.php?index=iaprtc12&cls=3" ;
+	data.userProfileServerUrl = "profile/"; //"http://vision.iti.gr/sotiris/isearch/client/musebag/user.php?mode=Profile&key=" ;
+	data.userLoginServerUrl = "login" ;
 	data.tagServerUrl = "http://vision.iti.gr/sotiris/isearch/client/musebag/user.php?mode=tags&index=iaprtc12" ;
-}
+};
