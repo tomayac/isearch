33660bab7c4f9d8e1db9771899ab471fd5e0be47
Jonas Etzold <jonas.etzold@fh-erfurt.de>
2012-02-03 10:14:46 +0100

    untrack local.js

--- client/musebag/js/mylibs/local.js
+++
@@ -1,9 +0,0 @@
-function localConfig(data)
-{
-	data.visOptions.method = 'classic' ;
-	data.fileUploadServer = "http://vision.iti.gr/sotiris/isearch/upload.php" ; //"query/item"; //
-	data.queryFormulatorUrl = "http://vision.iti.gr/sotiris/isearch/mqf.php?index=uc6"; //"query"; //"http://vision.iti.gr/sotiris/isearch/client/musebag/mqf.php?index=iaprtc12&cls=3" ;
-	data.userProfileServerUrl = "profile/"; //"http://vision.iti.gr/sotiris/isearch/client/musebag/user.php?mode=Profile&key=" ;
-	data.userLoginServerUrl = "login" ;
-	data.tagServerUrl = "http://vision.iti.gr/sotiris/isearch/client/musebag/user.php?mode=tags&index=iaprtc12" ;
-};
