baee59dfe1a5c83a6d50773eba0cbdb15ef49a44
malasiot <malasiot@iti.gr>
2012-02-06 13:15:49 +0200

    removing local.js from repo

--- client/musebag/js/mylibs/local.js
+++
@@ -1,9 +0,0 @@
-function localConfig(data)
-{
-	data.visOptions.method = 'classic' ;
-	data.fileUploadServer = "http://vision.iti.gr/sotiris/isearch/upload.php" ;
-	data.queryFormulatorUrl = "http://vision.iti.gr/sotiris/isearch/mqf.php?index=iaprtc12&cls=3" ;
-	data.userProfileServerUrl = "http://vision.iti.gr/sotiris/isearch/user.php?mode=Profile&key=" ;
-	data.userLoginServerUrl = "http://vision.iti.gr/sotiris/isearch/user.php?mode=login" ;
-	data.tagServerUrl = "http://vision.iti.gr/sotiris/isearch/user.php?mode=tags&index=iaprtc12" ;
-}
