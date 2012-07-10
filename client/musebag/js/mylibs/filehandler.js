53e5fe2ab239fcbd53ed69e4afcf73d87b2e0467
smirea <steven.mirea@gmail.com>
2012-07-10 12:41:04 +0200

    updated visualWords slider with zoom feature

--- client/musebag/js/mylibs/filehandler.js
+++ client/musebag/js/mylibs/filehandler.js
@@ -239,70 +239,7 @@ define("mylibs/filehandler", ["mylibs/loader", "libs/glge-compiled-min"], functi
       						pictureIcon = $('nav li[data-mode="video"]');
       						genericItemType = 'VideoType';
 
-                  /*HACK: this is just a temporary hack to develop the UI for
-                   * the video upload.
-                   */
-                  $.ajax({
-                    type: 'GET',
-                    url: 'dummy_video_response.xml',
-                    dataType: 'xml',
-                    success: function(xml){
-                      console.log('Loaded dummy xml file');
-                      var $root = $(xml).children().eq(0);
-                      var $videoKeyframes = $('#videoKeyframes');
-                      var imageData = [];
-                      $root
-                        .children()
-                        .each(function(){
-                          var $self = $(this).children().eq(0);
-                          var getAttr = function(attr){
-                            return $self.attr(attr);
-                          };
-                          imageData.push({
-                            uri: getAttr('uri'),
-                            score: parseInt(getAttr('score')),
-                            width: parseInt(getAttr('xMax') - getAttr('xMin')),
-                            height: parseInt(getAttr('yMax') - getAttr('yMin'))
-                          });
-                        });
-                      var $containers = $();
-                      var maxWidth = 80;
-                      var maxHeight = 80;
-                      for(var i in imageData){
-                        var $keyframeContainer = $(document.createElement('span'));
-                        //var width = Math.min(imageData[i].width, maxWidth);
-                        //var height = Math.min(imageData[i].height, maxHeight);
-                        $keyframeContainer
-                          .attr({
-                            score: imageData[i].score,
-                            uri: imageData[i].uri
-                          })
-                          .css({
-                            display: 'inline-block',
-                            border: '1px solid #ccc',
-                            background: "#fff url('"+imageData[i].uri+"') no-repeat",
-                            backgroundSize: 'cover',
-                            //backgroundPosition: '50% 50%',
-                            width: maxWidth+'px',
-                            height: maxHeight+'px',
-                            margin: '2px',
-                            padding: '2px'
-                          });
-                        $containers = $containers.add($keyframeContainer);
-                      }
-                      $containers.attr('class', 'keyframeContainer');
-                      $videoKeyframes
-                        .show()
-                        .html($containers)
-                        .swipePanel('remove')
-                        .swipePanel({
-                          scroll: 40
-                        })
-                        .bind('swipePanel-move', function(event){
-                          //TODO: actually preload nearby images
-                        });
-                    }
-                  });
+                  setupVideoKeyframeSelector();
       					}
 
       					var ele = $('#' + id) ;
@@ -359,9 +296,151 @@ define("mylibs/filehandler", ["mylibs/loader", "libs/glge-compiled-min"], functi
       }
 	    xhr.open("POST", this.serverURL, true);
 
-		loader.start("Uploading") ;
+	  loader.start("Uploading") ;
 
-	    xhr.send(formData);
+	  xhr.send(formData);
+
+    function setupVideoKeyframeSelector () {
+      var $videoKeyframes = $('#videoKeyframes');
+      /*HACK: this is just a temporary hack to develop the UI for
+       * the video upload.
+       */
+      $.ajax({
+        type: 'GET',
+        url: 'dummy_video_response.xml',
+        dataType: 'xml',
+        success: function(xml){
+          console.log('Loaded dummy xml file');
+          var $root = $(xml).children().eq(0);
+          var imageData = [];
+          $root
+            .children()
+            .each(function(){
+              var $self = $(this).children().eq(0);
+              var getAttr = function(attr){
+                return $self.attr(attr);
+              };
+              imageData.push({
+                uri: getAttr('uri'),
+                score: parseInt(getAttr('score')),
+                width: parseInt(getAttr('xMax') - getAttr('xMin')),
+                height: parseInt(getAttr('yMax') - getAttr('yMin'))
+              });
+            });
+          var $containers = $();
+          var maxWidth = 80;
+          var maxHeight = 80;
+          for(var i in imageData){
+            var $keyframeContainer = $(document.createElement('span'));
+            //var width = Math.min(imageData[i].width, maxWidth);
+            //var height = Math.min(imageData[i].height, maxHeight);
+            $keyframeContainer
+              .attr({
+                score: imageData[i].score,
+                uri: imageData[i].uri,
+                imageWidth: imageData[i].width,
+                imageHeight: imageData[i].height
+              })
+              .css({
+                position: 'relative',
+                display: 'inline-block',
+                border: '1px solid #ccc',
+                background: "#fff url('"+imageData[i].uri+"') no-repeat",
+                backgroundSize: 'cover',
+                width: maxWidth+'px',
+                height: maxHeight+'px',
+                margin: '2px',
+                padding: '2px'
+              })
+              .bind('click.zoom', visualWordZoom);
+            $containers = $containers.add($keyframeContainer);
+          }
+          $containers.attr('class', 'keyframeContainer');
+          $videoKeyframes
+            .show()
+            .html($containers)
+            .swipePanel('remove')
+            .swipePanel({
+              scroll: 40
+            })
+            .bind('swipePanel-move', function(event){
+              //TODO: actually preload nearby images
+            });
+        }
+      });
+
+      var visualWordZoom = (function(){
+        var $lastZoom = null;
+
+        $(document).bind('click.hideZoom', function(event){
+          if ($lastZoom) {
+            var $callTree = $(event.target).parents().add($(event.target));
+            if ($callTree.filter($lastZoom).length == 0
+                && $callTree.filter('.visualWordZoom').length == 0){
+              hideZoom($lastZoom);
+            }
+          }
+        });
+
+        return function visualWordZoom (event) {
+          $(this).addClass('visualWordZoom');
+          var container = $(document.createElement('div'));
+          var width = $(this).width();
+          var height = $(this).height();
+          var src = $(this).attr('uri');
+          var offset = $(this).offset();
+          container
+            .appendTo($('body'))
+            .css({
+              position: 'absolute',
+              zIndex: 100000,
+              left: offset.left,
+              top: offset.top,
+              border: '1px solid #bbb',
+              background: 'rgba(0, 0, 0, 0.8)',
+              width: width,
+              height: height,
+              opacity: 0.1
+            })
+            .animate({
+              left: offset.left - ($(this).attr('imageWidth') - width) / 2,
+              top: offset.top - ($(this).attr('imageHeight') - height) / 2,
+              width: $(this).attr('imageWidth'),
+              height: $(this).attr('imageHeight'),
+              opacity: 1
+            }, 500, function(){
+              var image = $(document.createElement('img'));
+              image
+                .attr('src', src)
+                .appendTo(container)
+                .hide()
+                .fadeIn(400);
+            });
+            hideZoom($lastZoom);
+            $lastZoom = container;
+            $videoKeyframes.swipePanel('pause');
+        };
+
+        function hideZoom (){
+          $videoKeyframes.swipePanel('unpause');
+          if ($lastZoom && $lastZoom.jquery) {
+            var offset = $lastZoom.offset();
+            $lastZoom.find('img').remove();
+            $lastZoom
+              .stop()
+              .animate({
+                left: offset.left + $lastZoom.width() / 2 - 1,
+                top: offset.top + $lastZoom.height() / 2 - 1,
+                width: 2,
+                height: 2,
+                opacity: 0
+              }, 300, function(){
+                $(this).remove();
+              });
+          }
+        }
+      })();
+    }
 
 	};
 
