31a360706101c6f105a1663f489dc8f80e10ba69
malasiot <malasiot@iti.gr>
2012-07-17 15:49:55 +0300

    plain visualisation script

--- client/musebag/js/local/uc1.js
+++ client/musebag/js/local/uc1.js
@@ -5,6 +5,8 @@ function localConfig(data)
 	data.visOptions.thumbOptions.iconArrange = "list" ;
 	
 	data.visOptions.filterBar.modalities =  { "audio": { label: "Audio Files" }  }
+	
+	data.fileUploadServer = "http://localhost/isearch/server/scripts/upload.php" ;
 	data.queryFormulatorUrl = "http://localhost/isearch/server/scripts/mqf.php?index=uc1" ;
 	data.userProfileServerUrl = "http://localhost/isearch/server/scripts/user.php?mode=Profile&key=" ;
 	data.userLoginServerUrl = "http://localhost/isearch/server/scripts/user.php?mode=login" ;
--- client/musebag/js/vis.js
+++ client/musebag/js/vis.js
@@ -8,40 +8,77 @@ if(typeof console == "undefined") {
   };
 }
 
-require(["jquery", 
-         "mylibs/menu",
+//Initialize the authentication component
+//----------------------------------------------------------
+if (typeof window.janrain !== 'object') window.janrain = {};
+window.janrain.settings = {};
+janrain.settings.tokenUrl = 'http://localhost';
+janrain.settings.tokenAction='event';
+//----------------------------------------------------------
+
+var Timeline_urlPrefix   = "js/libs/timeline_2.3.0/timeline_js/" ;
+var Timeline_parameters  = "bundle=false";
+var SimileAjax_urlPrefix = "js/libs/timeline_2.3.0/timeline_ajax/" ;
+
+require(["jquery",
          "mylibs/config",
          "mylibs/tags",
          "mylibs/results",
-         "mylibs/uiiface", 
+         "mylibs/uiiface",
+         "mylibs/query",
+		 "mylibs/loader",
+		 "mylibs/uiiface-v1",
          "libs/jquery.tokeninput",
-         "libs/canvas-toBlob.min",
-         "libs/smiley-slider"], 
-    function($, menu, config, tags, results, uiiface) {
+         "libs/smiley-slider",
+         "http://widget-cdn.rpxnow.com/js/lib/isearch/engage.js"
+        ], 
+    function($,  config, tags, results, uiiface, query, loader) {
       
       $(function() {
+        
         console.log('In the start function');
+        
         $(document).ready(function(){
           
-          console.log('In the start function');
-          $(document).ready(function(){
-
-            //Resizing of the menu on load and when window resizes
-
-            var resizeTimer;
-            
-            $(window).resize(function() {
-                clearTimeout(resizeTimer);
-                resizeTimer = setTimeout(function() { menu.adjust(); }, 200);
-            });
-
-            //Initializes the settings panel
-            config.initPanel();
-            results.display(__queryParams.q);
+                   
+          //Initializes the settings panel
+          config.initPanel();
+          
+                
+          //Close button of the panel
+          $('.panel footer a').click(function(){
+            $('.panel').slideUp(200);
           });
+		      	
+          loader.create() ;
+    
+      	var mqfUrl = config.constants.queryFormulatorUrl || 'query' ;
+	  
+		  if ( __queryParams.q )  mqfUrl += "&q=" + __queryParams.q ;
+		  if ( __queryParams.total ) mqfUrl += '&total=' + __queryParams.total ;
+		  if ( __queryParams.cls ) mqfUrl += '&cls=' + __queryParams.cls ;
+		  if ( __queryParams.tr ) mqfUrl += '&tr=' + __queryParams.tr ;
+		  
+    	  $.ajax({
+    	    type: "POST",
+    	    crossDomain: true,
+    	    url:  mqfUrl,
+    	    contentType : "application/json; charset=utf-8",
+    	    dataType : "json",
+    	    success: function(data) {
+    	     	results.display(data) ;
+    	    },
+    	    error: function(jqXHR, error, object) {
+    	      data = {error: "the server gave me an invalid result."}; 
+    	    },
+    	    complete: function() {
+    	      $.event.trigger( "ajaxStop" );
+    	    }
+    	  });
+      
+    	
+ 	      		
         }); //end document.ready()
       }); //end anonymous function     
-    }
-);
-
-
+    } //end main module
+); //end require
