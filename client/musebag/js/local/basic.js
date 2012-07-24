f966820941752d8f9866a9f9e10ab7eb4c7f1355
Jonas Etzold <jonas.etzold@fh-erfurt.de>
2012-07-24 14:03:36 +0200

    Changed server urls also in basic config

--- client/musebag/js/local/basic.js
+++ client/musebag/js/local/basic.js
@@ -1,9 +1,9 @@
 function localConfig(data)
 {
 	data.visOptions.method = 'cubes';
-	data.fileUploadServer =  "http://vision.iti.gr/sotiris/isearch/upload.php"; //"query/item"; 
-	data.queryFormulatorUrl = "http://vision.iti.gr/sotiris/isearch/mqf.php?index=uc6"; //"query";  
+	data.fileUploadServer =  "http://vision.iti.gr/isearch/server/scripts/upload.php"; //"query/item"; 
+	data.queryFormulatorUrl = "http://vision.iti.gr/isearch/server/scripts/mqf.php?index=uc6"; //"query";  
 	data.userProfileServerUrl = "profile/"; 
 	data.userLoginServerUrl = "login" ;
-	data.tagServerUrl = "http://vision.iti.gr/sotiris/isearch/user.php?mode=tags&index=uc6"; //"pTag/tag" ;
+	data.tagServerUrl = "http://vision.iti.gr/isearch/server/scripts/user.php?mode=tags&index=uc6"; //"pTag/tag" ;
 };
